f038a568021cdd9c020ebc0ea936c00eed3beeaf
Егор Путинцев <daxtur@live.com>
2019-02-21 23:16:29 +0700

    1

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(57, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(239, 308);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(382, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Статус выполнения";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Назад";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 335);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(117, 57);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Далее";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Reqres.Api";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,20 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using xNet;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        string link = "https://reqres.in/api/users/";// строка со ссылкой на сервер 
+        int control = 1;//глобальная паременная-счетчик
+        const int per_page = 3;//количество элементов на странице
+
+        public dynamic json(string apilink)//для облегчения чтения кода вывел запрос на сервер отдельной функцией
+        {
+            try
+            {
+                var reqres = new HttpRequest();
+                HttpResponse response = reqres.Get(apilink);
+                return JObject.Parse(response.ToString());
+            }
+            catch
+            {
+                label1.Text = "Ошибка при загрузке данных";
+                return 0;
+            }
+        }
+
+        public void download (int point)//функция для получения пользователей от сервера
+        {
+
+            int total = json(link).total;// получаю общее количество пользователей
+            int i = new int();
+            for (i = point; i < point+per_page; i++)//цикл, реализующий постраничный вывод
+            {
+                if (i < 1)
+                {
+                    button1.Enabled = false;
+                    break;
+                }
+                link = link + i.ToString();
+                dynamic data = json(link);//парсинг пользователей выполняю при помощи JSON. 
+                string user = data.data.first_name + " " + data.data.last_name;
+                link = link.Remove(28, i.ToString().Length);
+                listBox1.Items.Add(user);
+                //проверяю, не выйдет ли параметр цикла за общее количество пользователей во время выполнения. Если выходит - выхожу из цикла и отключаю кнопку далее
+                if (i >= total)
+                {
+                    button2.Enabled = false;
+                    break;
+                }
+                if (i + per_page > total)
+                    button2.Enabled = false;                    
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            button1.Enabled = false;
+            label1.Text = "Статус выполнения: идет обработка обработка данных";
+            download(control);
+            label1.Text = "Статус выполнения: обработка завершена";
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            button1.Enabled = true;//при нажатии на кнопку "Далее" активируется кнопка "Назад", и наоборот
+            listBox1.Items.Clear();
+            label1.Text = "Статус выполнения: идет обработка данных";
+            control = control + per_page;
+            download(control);
+            label1.Text = "Статус выполнения: обработка завершена";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            label1.Text = "Статус выполнения: идет обработка данных";
+            listBox1.Items.Clear();
+            control = control - per_page;
+            download(control);
+            if (control <= per_page) button1.Enabled = false;//если программа доберется до первой группы пользователей, кнопка деактивируется
+            label1.Text = "Статус выполнения: обработка завершена";
         }
     }
 }
